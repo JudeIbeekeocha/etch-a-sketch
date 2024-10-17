bedfd6529331bb42d1e318e263578c85ecd2e9de
Jude Ibe Ekeocha <153955257+JudeIbeekeocha@users.noreply.github.com>
2024-10-17 08:01:22 -0400

    implemented a dynamic pixel creation button
    Users can change the pixel size to their desire.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 100 * 100; i++) {
-  const divs = document.createElement("div");
-  divs.setAttribute("class", "child-container");
-  container.appendChild(divs);
-}
+const button = document.createElement("button");
+button.textContent = "Click me";
+container.appendChild(button);
 
-const childContainer = document.querySelectorAll('.child-container')
+button.onclick = () => {
+  squarePerSide = Number(prompt("How many squares per side? "));
 
-childContainer.forEach(child =>{
-    child.addEventListener('mouseover', () =>{
-        child.setAttribute('style', 'background-color: black')
 
-        setTimeout(() =>{
-        child.setAttribute('style', 'background-color: white')
-            
-        }, 1000)
-    })
-})
+  for (let i = 0; i < squarePerSide * squarePerSide; i++) {
+    const divs = document.createElement("div");
+    divs.classList.add('child-container')
+    // divs.setAttribute("class", "child-container");
+
+    const height = 100 / squarePerSide
+    const width = 100 / squarePerSide
+    divs.style.height = `${height}%` 
+    divs.style.width = `${width}%` 
+    divs.style.backgroundColor = 'white'
+    container.appendChild(divs);
+
+  }
+
+  const childContainer = document.querySelectorAll(".child-container");
+
+  childContainer.forEach((child) => {
+    child.addEventListener("mouseover", () => {
+    //   child.setAttribute("style", "background-color: black");
+      child.style.backgroundColor = 'black'
+
+      setTimeout(() => {
+        child.style.backgroundColor = 'white'
+      }, 1000);
+    });
+  });
+};
